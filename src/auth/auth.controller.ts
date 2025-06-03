bcfac6b5b29ed36b6e174e9917513e976af16c3b
Oluwadamilare Michael <63959057+michaeloluwadamilare@users.noreply.github.com>
2025-06-03 13:42:38 +0100

    passport & jwt auth module completed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login-auth.dto';
+import { LocalGuard } from './guards/local.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -8,6 +9,7 @@ export class AuthController {
     constructor(private readonly authService: AuthService){}
 
     @Post('login')
+    @UseGuards(LocalGuard)
     async login(@Body() loginDto: LoginDto){
         return await this.authService.authenticate(loginDto)
     }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,15 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LocalStrategy } from './strategies/local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
   imports: [
     UsersModule,
+    PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
