cdb8f6129a9792b52818c4a23f48749762af8cc7
Oluwadamilare Michael <63959057+michaeloluwadamilare@users.noreply.github.com>
2025-06-12 23:23:37 +0100

    Update role.guard.ts

--- src/auth/guards/role.guard.ts
+++ src/auth/guards/role.guard.ts
@@ -4,7 +4,7 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 export class RolesGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
-    const user = request.user;
-    return user.user?.role === 'ADMIN';
+    const user = request.user.user;
+    return user?.role === 'ADMIN';
   }
 }
